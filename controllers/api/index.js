3c0623ddd51696bbf1ff7d4dd796392b650b4cfa
Amy Lloyd <amylloyd30@gmail.com>
2024-07-09 13:51:14 +0930

    delete and create item routes successful

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const adminRoutes = require('./adminRoutes');
 const categoryRoutes = require('./categoryRoutes');
+const itemRoutes = require('./itemRoutes');
 
 router.use('/admins', adminRoutes);
 router.use('/categories', categoryRoutes);
+router.use('/items', itemRoutes);
 
 module.exports = router;
