6cc06db077d0d6650065011f3e2e7c64eafa9bed
Amy Lloyd <amylloyd30@gmail.com>
2024-07-31 10:18:07 +0930

    small adjustment for heroku deploy

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-// const helpers = require('./utils/helpers');
 const routes = require('./controllers');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sequelize = require('./config/connection');
