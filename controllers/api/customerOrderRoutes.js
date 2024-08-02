854b12511a2c7034534cd3faae2c93964c480dc5
Amy Lloyd <amylloyd30@gmail.com>
2024-08-02 18:51:42 +0930

    debug customerorder route WIP

--- controllers/api/customerOrderRoutes.js
+++ controllers/api/customerOrderRoutes.js
@@ -8,6 +8,11 @@ const withAuth = require('../../utils/auth');
 router.post('/data/:id', async (req, res) => {
   const { items, sum, cartRef, cartLocation  } = req.body; // Expecting an array of item IDs and the total amount
   let quantity = 0;
+  console.log(req.body.items, "req.body.items");
+  console.log(req.body.sum, "req.body.sum");
+  console.log(req.body.cartRef, "req.body.cartRef");
+  console.log(req.body.cartLocation, "req.body.cartLocation");
+  console.log(req.params.id, "req.params.id");
 
   if (!items || !sum || !cartRef || !cartLocation ) {
     return res.status(400).json({ error: 'All details are required' });
